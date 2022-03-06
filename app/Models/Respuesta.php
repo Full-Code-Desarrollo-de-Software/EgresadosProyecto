61b25e973a9ecadd36153c37aeaab86c3b3ffcff
YadiraIsabel <l19450664@itz.edu.mx>
2022-03-05 22:22:11 -0600

    Modelos egresado, pregunta, respuesta con migraciones creados

--- app/Models/Respuesta.php
+++ app/Models/Respuesta.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Respuesta extends Model
 {
     use HasFactory;
+
+    public function egresado (){
+        return $this->belongsTo('App\Models\Egresado', 'egresado_id');
+    }
+
+    public function pregunta (){
+        return $this->belongsTo('App\Models\Pregunta', 'pregunta_id');
+    }
 }
